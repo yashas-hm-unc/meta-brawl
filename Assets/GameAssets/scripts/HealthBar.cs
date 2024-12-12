c620b997070024adc9c76b10330753a9105c5788
Paul Parker <pparker@unc.edu>
2024-12-12 14:09:18 -0500

    Health bar prefab

---
+++ Assets/GameAssets/scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
