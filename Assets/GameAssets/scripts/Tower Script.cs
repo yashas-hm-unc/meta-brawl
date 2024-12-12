071e54bab09b99420d49367c000442ffefeb3a1b
Yashas H Majmudar <yashashm.dev@gmail.com>
2024-12-11 22:15:46 -0500

    Arena bounds, Flamethrower, tower blast and fade destroy, spatial audio

--- Assets/GameAssets/scripts/Tower Script.cs
+++ Assets/GameAssets/scripts/Tower Script.cs
@@ -1,35 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TowerScript : MonoBehaviour
 {
     public GameObject originalTower;
     public GameObject brokenTower;
+    public ParticleSystem blastParticles;
+    public AudioClip blastSound;
+    public AudioSource audioSource;
     public int damageTaken = 0;
+    private bool _blast = false;
+
+    private void Start()
+    {
+        brokenTower.SetActive(false);
+    }
 
     void Update()
     {
-        if (damageTaken >= 100)
+        if (!_blast && damageTaken >= 100 && originalTower is not null && brokenTower is not null)
         {
-            if (originalTower != null)
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        _blast = true;
+        originalTower.SetActive(false);
+        brokenTower.SetActive(true);
+        // brokenTower = Instantiate(brokenTower, originalTower.transform.position, originalTower.transform.rotation);
+        Rigidbody[] rigidbodies = brokenTower.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            if (rb is not null)
             {
-                originalTower.SetActive(false);
-                if (brokenTower != null)
+                rb.AddExplosionForce(2, originalTower.transform.position, 2f);
+            }
+        }
+
+        audioSource.clip = blastSound;
+        audioSource.Play();
+        blastParticles.Play();
+
+        StartCoroutine(FadeOutDestroyedObjects(rigidbodies));
+    }
+
+    private IEnumerator FadeOutDestroyedObjects(Rigidbody[] rigidbodies)
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        int activeBodies = rigidbodies.Length;
+        while (activeBodies > 0)
+        {
+            yield return wait;
+            foreach (Rigidbody rb in rigidbodies)
+            {
+                if (rb.IsSleeping())
                 {
-                    Vector3 pos = originalTower.transform.position;
-                    pos.y -= pos.y;
-                    brokenTower = Instantiate(brokenTower, pos, originalTower.transform.rotation);
-                    foreach (Transform t in brokenTower.transform)
-                    {
-                        var rb = t.GetComponent<Rigidbody>();
-                        if (rb != null)
-                        {
-                            rb.AddExplosionForce(2, originalTower.transform.position, 2);
-                        }
-                    }
+                    activeBodies--;
                 }
             }
         }
+
+        yield return new WaitForSeconds(5f);
+
+        float time = 0;
+        Renderer[] renderers = Array.ConvertAll(rigidbodies, GetRenderer);
+
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            Destroy(rb.GetComponent<Collider>());
+            Destroy(rb);
+        }
+
+        while (time < 1)
+        {
+            float step = Time.deltaTime * 0.25f;
+            foreach (Renderer renderer in renderers)
+            {
+                renderer.transform.Translate(Vector3.down * (step / renderer.bounds.size.y), Space.World);
+            }
+
+            time += step;
+            yield return null;
+        }
+
+        foreach (Renderer renderer in renderers)
+        {
+            Destroy(renderer.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+
+    private Renderer GetRenderer(Rigidbody body)
+    {
+        return body.GetComponent<Renderer>();
     }
 }
