20228c1688450a260d6e7a4ac37a252857bede58
Paul Parker <pparker@unc.edu>
2024-12-12 14:43:12 -0500

    Stamina bar does not have a gradient

--- Assets/GameAssets/scripts/StaminaBar.cs
+++ Assets/GameAssets/scripts/StaminaBar.cs
@@ -3,24 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class StaminaBar : MonoBehaviour
 {
     public Slider slider;
-    public Gradient gradient;
     public Image fill;
 
 
-    public void SetHealth(int health)
+    public void SetStamina(int stamina)
     {
-        slider.value = health;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        slider.value = stamina;
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        fill.color = gradient.Evaluate(1f);
     }
 
     // Update is called once per frame
