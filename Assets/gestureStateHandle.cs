dbb6de2aae15111a2d3478f99fee999b1deab80f
kratty73 <karthikeya.jatoth@gmail.com>
2024-12-12 21:24:50 -0500

    gestures tested

--- Assets/gestureStateHandle.cs
+++ Assets/gestureStateHandle.cs
@@ -1,31 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class gestureStateHandler : MonoBehaviour
+public class gestureStateHandle : MonoBehaviour
 {
     // Start is called before the first frame update
     // 0=>thumbsUpRight, 1=>thumbsDownRight, 2=>thumbsUpLeft, 3=>thumbsDownLeft, 4=>fistRight, 5=>fistLeft, 6=>pointRight, 7=>pointLeft, 8=>clawRight, 9=>clawLeft,
     private bool[] gestureState = { false, false, false, false, false, false, false, false, false, false };
-    //public TMP_Text mainObject;
+    // public GameObject mainObject;
     private string a = "";
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        //a = "";
-        //for(int i=0; i< 10; i++)
-        //{
-        //    if (gestureState[i])
-        //    {
-        //        a += i.ToString()+" ";
-        //    }
-        //}
-        //mainObject.text = a + "gestures made";
+        a = "";
+        for (int i = 0; i < 10; i++)
+        {
+            if (gestureState[i])
+            {
+                a += i.ToString();
+            }
+        }
+        //mainObject.text = "a";
     }
 
     public void updateGestureStateTrue(int a)
