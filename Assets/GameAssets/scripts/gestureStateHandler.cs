915c293fbf01c101601d7e04ba13ef09a4799ae1
kratty73 <karthikeya.jatoth@gmail.com>
2024-12-13 00:04:05 -0500

    added rasengan on gesture

--- Assets/GameAssets/scripts/gestureStateHandler.cs
+++ Assets/GameAssets/scripts/gestureStateHandler.cs
@@ -8,34 +8,64 @@ public class gestureStateHandler : MonoBehaviour
     // Start is called before the first frame update
     // 0=>thumbsUpRight, 1=>thumbsDownRight, 2=>thumbsUpLeft, 3=>thumbsDownLeft, 4=>fistRight, 5=>fistLeft, 6=>pointRight, 7=>pointLeft, 8=>clawRight, 9=>clawLeft,
     private bool[] gestureState = { false, false, false, false, false, false, false, false, false, false };
-    //public TMP_Text mainObject;
-    private string a = "";
-    void Start()
-    {
-        
-    }
+    private GameObject[] gameObjects = { null, null, null, null, null, null, null, null, null, null };
 
-    void Update()
+    public GameObject palmHolderLeft;
+    public GameObject palmHolderRight;
+    public GameObject Rasengan;
+    public TMP_Text mainObject;
+    private int count = 0;
+
+    void FixedUpdate()
     {
-        //a = "";
-        //for(int i=0; i< 10; i++)
-        //{
-        //    if (gestureState[i])
-        //    {
-        //        a += i.ToString()+" ";
-        //    }
-        //}
-        //mainObject.text = a + "gestures made";
+        string a = "";
+        for(int i=0; i< 10; i++)
+        {
+            if (gestureState[i])
+            {
+                a += i.ToString()+" ";
+            }
+        }
+        mainObject.text = a + count.ToString() + "gestures made";
+
     }
 
     public void updateGestureStateTrue(int a)
     {
         gestureState[a] = true;
+        if (a == 8)
+        {
+            GameObject rasengan_right = Instantiate(Rasengan, new Vector3(0, 0, 0), Quaternion.identity);
+            gameObjects[8] = rasengan_right;
+            rasengan_right.transform.SetParent(palmHolderRight.transform);
+            rasengan_right.transform.localPosition = new Vector3(0, -0.1f, 0);
+            rasengan_right.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            count++;
+        }
+        if (a == 9)
+        {
+            GameObject rasengan_left = Instantiate(Rasengan, new Vector3(0, 0, 0), Quaternion.identity);
+            gameObjects[9] = rasengan_left;
+            rasengan_left.transform.SetParent(palmHolderLeft.transform);
+            rasengan_left.transform.localPosition = new Vector3(0, 0.1f, 0);
+            rasengan_left.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            count++;
+        }
     }
 
     public void updateGestureStateFalse(int a)
     {
         gestureState[a] = false;
+        if (a == 8)
+        {
+            Destroy(gameObjects[8]);
+            count--;
+        }
+        if (a == 9)
+        {
+            Destroy(gameObjects[9]);
+            count--;
+        }
     }
 
     public bool getGestureState(int a)
