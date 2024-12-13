8732405b99feb8d7fce05173941cc62ae0a89932
kratty73 <karthikeya.jatoth@gmail.com>
2024-12-13 06:46:04 -0500

    multi-player-character-done

---
+++ Assets/GameAssets/scripts/vrRigReference.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vrRigReference : MonoBehaviour
+{
+    public Transform root;
+    public static vrRigReference Singleton;
+
+    private void Awake()
+    {
+        Singleton = this;
+    }
+}
